77e09ababf708b93f135ee3c2000f8b15f4e9c1c
rk8287 <rounak8287@gmail.com>
2024-08-06 12:57:25 +0530

    change-commit

--- src/component/join/Join.js
+++ src/component/join/Join.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import "./Join.css"
 const Join = () => {
   return (
@@ -8,9 +9,9 @@ const Join = () => {
         <h3 className='h3'>Join Us as a Partner Driver</h3>
         <p className='p2'>Are you a vehicle owner looking for new opportunities? Join MyFastX today and become part of a dynamic network of drivers. With us, you’ll enjoy flexible work hours, competitive earnings, and the satisfaction of delivering smiles across the city.
 </p>
-
+<Link to={'/payment'}>
 <button className='b1'>Register now for just Rs. 999!
-</button>
+</button></Link>
         </div>
     </div>
   )
